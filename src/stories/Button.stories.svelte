fbbcfc9071d53e38af998fe67ffbd0ab4a14e64a
jwalke18 <jwalke18@jaguarlandrover.com>
2022-05-18 19:39:36 +0100

    refactor: init project component library

---
+++ src/stories/Button.stories.svelte
@@ -0,0 +1,64 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import Button from "../lib/Button.svelte";
+</script>
+
+<Meta
+  title="Button"
+  component={Button}
+  argTypes={{
+    label: { control: "text" },
+    primary: { control: "boolean" },
+    size: {
+      control: { type: "select", options: ["small", "medium", "large"] },
+    },
+    onClick: { action: "onClick" },
+  }}
+/>
+
+
+<Template let:args>
+  <Button {...args} on:click={args.onClick} />
+</Template>
+
+
+<Story
+  name="Primary"
+  args={{
+    primary: true,
+    label: "Button",
+  }}
+/>
+
+<Story
+  name="Secondary"
+  args={{
+    primary: false,
+    label: "Button",
+  }}
+/>
+
+<Story
+  name="Large"
+  args={{
+    size: "large",
+    label: "Button",
+  }}
+/>
+
+
+<Story
+  name="Medium"
+  args={{
+    size: "medium",
+    label: "Button",
+  }}
+/>
+
+<Story
+  name="Small"
+  args={{
+    size: "small",
+    label: "Button",
+  }}
+/>
